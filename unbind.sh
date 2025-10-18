24ae6d61ef1542ca9acc98a152645b8f24707d7f
Snekutaren <snekutaren@gmail.com>
2025-10-18 23:46:16 +0200

    first commit

---
+++ unbind.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+hyprctl dispatch exit
+sleep 2
+
+echo 0000:03:00.0 > /sys/bus/pci/drivers/amdgpu/unbind
+sleep 2
+
+#echo 0000:03:00.1 > /sys/bus/pci/drivers/snd_hda_intel/unbind
+#sleep 2
+
+echo 3 > /sys/bus/pci/devices/0000:03:00.0/resource2_resize
+sleep 2
+
+#Hyprland
